dded4f622b74b52dc126001451f7b711044de7fd
Daniyar Burakaev <daniyar.burakaev@mail.ru>
2021-05-03 17:08:44 +0300

    frst

---
+++ src/main/java/dan/spring5web/guru1/Application.java
@@ -0,0 +1,13 @@
+package dan.spring5web.guru1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
